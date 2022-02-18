c4972899abeb397de2b7c280d4d3ee3f7d4471be
Triston Burns <tristonburns97@gmail.com>
2022-02-18 09:50:43 -0700

    Init commit

---
+++ src/main/java/com/example/HerokuApp/HerokuAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.HerokuApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HerokuApp/HerokuAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HerokuApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
